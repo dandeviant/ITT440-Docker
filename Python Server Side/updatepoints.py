950b8b3d64da811c48bc13b437955bbf980c5968
Matt Daniel <68473358+dandeviant@users.noreply.github.com>
2022-02-15 22:12:48 +0800

    Update updatepoints.py

--- Python Server Side/updatepoints.py
+++ Python Server Side/updatepoints.py
@@ -12,50 +12,52 @@ except ImportError:
     print("\n\n Restart the program")
     exit()
 
+
+hostip = "172.19.0.2"
+
+
 mydb = mysql.connector.connect(
-  host="172.19.0.4",
-  user="root",
+  host= hostip,
+  user= "root",
   password="mysql",
   database="users"
 )
 
-user = "Python Server"
-
 try:
-    mydb = mysql.connector.connect(
-    host="172.19.0.4",
-    user="root",
-    password="mysql",
-    database="users"
-    )
-    cont = 1
-    cursor = mydb.cursor()
-    sql = "SELECT * FROM user WHERE origin_server='%s' "%(user)
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    while cont == 1:
-        time.sleep(1)
-        date = str(os.popen('date').read())
-        datetime_stamp = date.strip()
-        cursor = mydb.cursor()
-        sql = "UPDATE user SET points=points+1, datetime_stamp ='%s' WHERE origin_server='Python Server'"%(datetime_stamp)
-        cursor.execute(sql)
-        mydb.commit()
-        cursor.execute("SELECT user, points FROM user WHERE origin_server='Python Server' ")
-        myresult = cursor.fetchall()
-        print("Time of update: ", datetime_stamp)
-        print("=========================")
-        for x in myresult:
-            print("User      : ", x[0])
-            print("Points    : ", x[1])
-            print("=========================")
-        print("\n")
-        cont = 1
+	mydb = mysql.connector.connect(
+ 	host= hostip,
+ 	user="root",
+ 	password="mysql",
+  	database="users"
+	)
+	cont = 1
+	cursor = mydb.cursor()
+	sql = "SELECT * FROM user WHERE origin_server='Python Server' "
+	cursor.execute(sql)
+	result = cursor.fetchall()
+	while cont == 1:
+		time.sleep(5)
+		date = str(os.popen('date').read())
+		datetime_stamp = date.strip()
+		cursor = mydb.cursor()
+		sql = "UPDATE user SET points=points+1, datetime_stamp ='%s' WHERE origin_server='Python Server'"%(datetime_stamp)
+		cursor.execute(sql)
+		mydb.commit()
+		cursor.execute("SELECT user, points FROM user WHERE origin_server='Python Server' ")
+		myresult = cursor.fetchall()
+		print("Time of update: ", datetime_stamp)
+		print("=========================")
+		for x in myresult:
+			print("User      : ", x[0])
+			print("Updated Points    : ", x[1])
+			print("=========================")
+		print("\n")
+		cont = 1
 
 except KeyboardInterrupt:
-    date = str(os.popen('date').read())
-    datetime_stamp = date.strip()
-    print("\nStopping Program\n");
-    time.sleep(1)
-    print("Final Points Updated\n\n")
-    exit()
+	date = str(os.popen('date').read())
+	datetime_stamp = date.strip()
+	print("\nStopping Program\n");
+	time.sleep(1)
+	print("Final Points Updated\n\n")
+	exit()
